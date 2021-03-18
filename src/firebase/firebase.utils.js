174df386b6b97dbc14ecb2e81b6aaaa95a75d3a6
Lenny <riel_lenny@yahoo.co.uk>
2021-03-18 13:14:46 +0200

    implemented Firebase utils for sign up component/styles

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,6 +14,28 @@ const config = {
   measurementId: "G-X5E6PRT6GT",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  // If there is a userAuth object, query inside firestore for the document to see if it already exists in database.
+
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  // If user does not exist, create a new user with these properties.
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({ displayName, email, createdAt, ...additionalData });
+    } catch (error) {
+      console.log("Error creating user", error.message);
+    }
+  }
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
